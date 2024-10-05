9a4cafa76f0e00283bbfbf05062c73010a77d66e
Alper <alperdevir@gmail.com>
2024-10-05 04:33:27 +0300

    Yeni dosya eklendi: TypeCsting1.java

---
+++ src/main/java/TypeCasting/TypeCasting01.java
@@ -0,0 +1,41 @@
+package TypeCasting;
+
+public class TypeCasting01 {
+    public static void main(String[] args) {
+
+    //1) Short data t!p!nde b!r deg!sken olusturunuz ve onu b!r !nt deg!sken!ne dönüstürünüz.
+
+        short yas = 30;
+        int yeniYas = yas;
+        System.out.println("Yeni Yas: " + yeniYas);
+
+        System.out.println("//-------------------------------------");
+
+       // 2) Long data t!p!nde b!r deg!sken olusturunuz ve onu b!r !nt deg!sken!ne dönüstürünüz.
+
+        long yas1= 12L;
+        int yeniYas1= (int) yas1;
+        System.out.println(yeniYas1);
+
+        System.out.println("//-------------------------------------");
+
+       // 3) Double data t!p!nde b!r deg!sken olusturunuz ve onu b!r float deg!sken!ne dönüstürünüz.
+
+        double fiyat= 1.2;
+        short yeniFiyat = (short) fiyat;
+        System.out.println(yeniFiyat);
+
+        System.out.println("//-------------------------------------");
+
+      //  4) Double data t!p!nde b!r deg!sken olusturunuz ve onu b!r short deg!sken!ne dönüstürünüz.Sonrasında bu short deg!sken!n deger!n! konsolda yazdırınız.
+        //  Not : Çıktıya dikkat ediniz, ondalık bir deger olmayacaktır.
+
+        double fiyat1= 4.5;
+
+
+
+
+
+
+    }
+}
